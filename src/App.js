d9c7706558717df0c8cf3212e09e4db288519c0d
caroko9 <caroko9@hotmail.com>
2023-10-21 02:31:50 -0300

    iniciando proyecto

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Escuela from '../src/components/Escuela';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+      </header>
+      <main>
+        <Escuela />
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Escuela.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Escuela() {
+    return (
+      <div>
+      <h3>ESCUELAS</h3>
+      </div>
+    );
+  }
+
+export default Escuela;
